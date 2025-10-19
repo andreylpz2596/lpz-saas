2500341f84e5bd0b7bc8e40e2a7a2fb2db9e28f8
andreylpz2596 <andreylpz2596@gmail.com>
2025-10-19 00:25:27 -0500

    fix: include ProductPdfGenerate package as local code

---
+++ packages/Brainstream/ProductPdfGenerate/src/Providers/ModuleServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Brainstream\ProductPdfGenerate\Providers;
+
+use Konekt\Concord\BaseModuleServiceProvider;
+
+class ModuleServiceProvider extends BaseModuleServiceProvider
+{
+    protected $models = [
+    ];
+}
